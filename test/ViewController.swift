151330f832b0a31c1544ac5ec124604e284e6042
nguyenngo6 <s3480522@rmit.edu.vn>
2018-08-29 11:07:14 +0700

    aaa

---
+++ test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Nguyen Ngo on 8/29/18.
+//  Copyright © 2018 Nguyen Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+    
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
